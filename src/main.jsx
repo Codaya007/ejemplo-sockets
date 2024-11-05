af02ffae2af2d06b01e2e66de3a011f929f279ee
Vivi <viviana.calva@lumation.co>
2024-11-05 14:45:01 -0500

    Adding channels admin

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import ChannelsDrivers from './ChannelsDrivers'
+import Channels from './Channels.jsx'
 // import AppSails from './AppSails.jsx'
 // import AppSocket from './AppSocket.jsx'
 // import Generaciones from './Generaciones.jsx'
 // import TestEvents from './TestEvents.jsx'
-// import Channels from './Channels.jsx'
 
 // import App from './AppChat.jsx'
 
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     {/* <AppSails /> */}
     {/* <AppSocket /> */}
     {/* <TestEvents /> */}
-    {/* <Channels /> */}
+    <Channels />
     <ChannelsDrivers />
     {/* <Generaciones /> */}
   </React.StrictMode>,
